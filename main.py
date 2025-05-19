cdc315348bb2fa7747b7c4d4f1f25f025fc75a1b
Joey Gonzales-Dones <joeygonzalesdones@gmail.com>
2025-05-19 17:56:04 -0400

    Parse the total number of items on the page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 
 PRICECHARTING_URL = "https://www.pricecharting.com/console/gamecube"
 
@@ -9,6 +10,8 @@ def main():
     driver.get(PRICECHARTING_URL)
     print(f"Driver title: {driver.title}")
     driver.implicitly_wait(2)
+
+    item_count = get_item_count(driver)
 
     games_table = driver.find_element(by=By.ID, value="games_table")
     games_table_body = games_table.find_element(by=By.TAG_NAME, value="tbody")
@@ -20,7 +23,27 @@ def main():
         for column_index, value in enumerate(row_data):
             print(f"* Column {column_index}: '{value.text}'")
 
+    print()
+    print(f"Total count: {item_count} items")
+
     driver.quit()
+
+
+def get_item_count(driver: WebDriver) -> int:
+    console_header = driver.find_element(by=By.ID, value="console-header")
+    console_header_elements = console_header.find_elements(by=By.TAG_NAME, value="p")
+
+    item_count = None
+    for element in console_header_elements:
+        if element.text.startswith("You own:"):
+            item_count = element.text.split(" / ")[1].split()[0]
+
+    if item_count is None:
+        raise RuntimeError('Item count element ("You own: ### / ### items") not found')
+    if not item_count.isdigit():
+        raise RuntimeError(f'Item count element "{item_count}" not a valid number')
+
+    return int(item_count)
 
 
 if __name__ == "__main__":
