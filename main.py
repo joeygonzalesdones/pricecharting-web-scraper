1aeaa4a2eee1c1e292db8cde88e23734225ee976
Joey Gonzales-Dones <joeygonzalesdones@gmail.com>
2025-05-19 17:30:56 -0400

    Load page and fetch raw data without scrolling

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+PRICECHARTING_URL = "https://www.pricecharting.com/console/gamecube"
+
 
 def main():
     driver = webdriver.Chrome()
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    driver.get(PRICECHARTING_URL)
     print(f"Driver title: {driver.title}")
+    driver.implicitly_wait(2)
 
-    driver.implicitly_wait(0.5)
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-    text_box.send_keys("Selenium")
-    submit_button.click()
+    games_table = driver.find_element(by=By.ID, value="games_table")
+    games_table_body = games_table.find_element(by=By.TAG_NAME, value="tbody")
+    games_table_rows = games_table_body.find_elements(by=By.TAG_NAME, value="tr")
 
-    message = driver.find_element(by=By.ID, value="message")
-    print(f"Message text: {message.text}")
+    for index, row in enumerate(games_table_rows):
+        print(f"-------------------- Row {index} --------------------")
+        row_data = row.find_elements(by=By.TAG_NAME, value="td")
+        for column_index, value in enumerate(row_data):
+            print(f"* Column {column_index}: '{value.text}'")
 
     driver.quit()
 
