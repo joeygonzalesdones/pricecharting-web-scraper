bcc4e250fecac0e8d329f1b619a066f97367eac2
Joey Gonzales-Dones <joeygonzalesdones@gmail.com>
2025-05-21 17:39:29 -0400

    Add docstrings

--- main.py
+++ main.py
@@ -14,7 +14,19 @@ PRICECHARTING_URL = f"https://www.pricecharting.com/console/{CONSOLE}?sort=popul
 
 @dataclass
 class GameData:
-    """Represents a video game item and its estimated prices."""
+    """Groups data related to a specific video game. All price values are given
+    in United States dollars (USD).
+
+    :param title: The game's title
+    :param game_page_url: URL of the game's dedicated page on PriceCharting
+    :param popularity_rank: The game's rank when sorted by popularity for its console
+    :param price_loose: The game's current market price in loose condition (cartridge/disk only)
+        as estimated by PriceCharting
+    :param price_cib: The game's current market price in complete-in-box condition
+        as estimated by PriceCharting
+    :param price_new: The game's current market price in brand-new condition
+        as estimated by PriceCharting
+    """
 
     title: str
     game_page_url: str
@@ -39,14 +51,15 @@ def main():
     total_games_loaded = len(game_table_rows)
 
     print(f"Loaded {total_games_loaded}/{game_count} games in {elapsed_time:.3f} seconds")
-    print(f"({elapsed_time/total_games_loaded} seconds/game)")
+    print(f"({elapsed_time / total_games_loaded} seconds/game)")
     print()
     print(f"First game text: '{game_table_rows[0].text}'")
     print(f"Last game text: '{game_table_rows[-1].text}'")
     print()
 
     start_time = time.time()
-    parsed_game_data = [parse_game_data(element, rank) for rank, element in enumerate(game_table_rows)]
+    parsed_game_data = [parse_game_data(element, rank) for rank, element in
+                        enumerate(game_table_rows)]
     elapsed_time = time.time() - start_time
     print(f"Parsed games in {elapsed_time:.3f} seconds")
     print()
@@ -58,11 +71,19 @@ def main():
 
 
 def get_game_count(driver: WebDriver) -> int:
+    """Parses the current game list page to find the total number of games available
+    when the page is fully loaded.
+
+    :param driver: The web browser controller
+    :raises RuntimeError: If the game count is missing or not a valid number
+    :return: The total number of games on the current page
+    """
+
     element = driver.find_element(by=By.XPATH, value="//*[@id='console-header']/p/b")
     game_count = element.text.split(" / ")[1].split()[0]
 
     if game_count is None:
-        raise RuntimeError('Game count ("You own: ### / ### items") not found')
+        raise RuntimeError('Game count ("You own: ___ / <game_count> items") not found')
     if not game_count.isdigit():
         raise RuntimeError(f'Game count "{game_count}" not a valid number')
 
@@ -70,6 +91,14 @@ def get_game_count(driver: WebDriver) -> int:
 
 
 def load_games_as_dom_elements(driver: WebDriver, game_count: int) -> List[WebElement]:
+    """Retrieves the DOM elements on the current page that correspond to the data entries
+    for each game.
+
+    :param driver: The web browser controller
+    :param game_count: The number of games to load
+    :return: A list of DOM elements, where each element contains data for a single game
+    """
+
     game_elements = driver.find_elements(by=By.XPATH, value="//*[@id='games_table']/tbody/tr")
     while len(game_elements) < game_count:
         print(f"{len(game_elements)} games loaded")
@@ -80,6 +109,13 @@ def load_games_as_dom_elements(driver: WebDriver, game_count: int) -> List[WebEl
 
 
 def parse_game_data(game_element: WebElement, popularity_rank: Optional[int] = None) -> GameData:
+    """Parses a specific game's DOM element into an object storing the relevant data.
+
+    :param game_element: A DOM element corresponding to an individual game
+    :param popularity_rank: The game's rank when sorted by popularity for its console
+    :return: An object storing only the relevant data for the game
+    """
+
     columns = game_element.find_elements(by=By.TAG_NAME, value="td")
     title_element = columns[1]
     title = title_element.text
@@ -91,6 +127,13 @@ def parse_game_data(game_element: WebElement, popularity_rank: Optional[int] = N
 
 
 def _parse_price_string(price_string: str) -> Optional[float]:
+    """Parses a string representing a price in USD into the corresponding floating point value.
+    For example, `_parse_price_string("$1,234.56")` should return `1234.56`.
+
+    :param price_string: A string representing a price in USD
+    :return: The corresponding floating point value, or None if the input string is empty
+    """
+
     if price_string:
         return float(price_string.lstrip("$").replace(",", ""))
     return None
